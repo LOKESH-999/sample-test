a06d4310a53677159133d0c02f010046d817a767
LOKESH-999 <lokiapm888@gmail.com>
2023-08-21 23:37:20 +0530

    added diagonal,resize

--- matrix/src/main.rs
+++ matrix/src/main.rs
@@ -3,8 +3,14 @@ mod matrix;
 fn main() {
     println!("Hello, world!");
     let mut  x=matrix::Matris::<i32>::ones((5,4));
-    println!("{:?}",x.reshape( (4,5),false ));
+    println!("{:?}",x.reshape( (4,5)));
     println!("{:?}",x);
     println!("{:?}",x.data());
-    println!("{:?}",x.squeeze())
+    println!("{:?}",x.squeeze());
+    x.resize((4,5));
+    println!("{:?}",x);
+    println!("{:?}",x.diagonal());
+    let x=vec![vec![1,2,3],vec![4,5,6],vec![7,8,9]];
+    println!("{:?}",x);
+    println!("{:?}",x);
 }
--- matrix/src/matrix.rs
+++ matrix/src/matrix.rs
@@ -29,14 +29,15 @@ impl<T:std::clone::Clone +std::fmt::Debug> Matris<T> {
     pub fn shape(&self)->(usize,usize){
         self.shape.clone()
     }
-    pub fn vec_tranform(d:Vec<Vec<T>>)->Matris<T>{   // MATRIX  must contain same no of element in each column
+    pub fn from_vec(d:Vec<Vec<T>>)->Matris<T>{   // MATRIX  must contain same no of element in each column
         let x= d.len();
         let y=d[0].len();
-        d.iter().map(|x|{
-            if x.len()!=y{panic!("PROVIDED SHAPE  OF MATRIX IS INCORRECT")}
-            x
-        });
-        Matris { data: d, shape: (0,9) }
+        for i in d.iter(){
+            if i.len()!=y{
+                panic!("PROVIDED SHAPE  OF MATRIX IS INCORRECT");
+            }
+        }
+        Matris { data: d, shape: (x,y) }
     }
     pub fn gen_numericals(value:T,shape:(usize,usize))->Matris<T>{
         if shape.0<1 || shape.1<1{panic!("PROVIDED SHAPE {:?} IS IN CORRECT </ SHAPE MUST BE GREATER_EQ THEN (1,1)/>",shape)}
@@ -58,12 +59,11 @@ impl<T:std::clone::Clone +std::fmt::Debug> Matris<T> {
         }
         v
     }
-    pub fn reshape(&mut self,shape:(usize,usize),inplace:bool)->Matris<T>{
+    pub fn reshape(&self,shape:(usize,usize))->Matris<T>{
         if self.shape.0*self.shape.1!=shape.0*shape.1{
             panic!("ENTERED SHAPE IS INVALID")
         }
         let mut v=Vec::with_capacity(shape.0*shape.1+2);
-        let (mut start_in,mut end_in)=(0 as usize,shape.1);
         let mut v1=Vec::<T>::with_capacity(shape.0+2);
         for (i,val) in self.squeeze().into_iter().enumerate(){
             v1.push(val);
@@ -71,8 +71,27 @@ impl<T:std::clone::Clone +std::fmt::Debug> Matris<T> {
                 v.push(v1.clone());
                 v1.clear();
             }
-            
         }
         Matris{data:v,shape:shape}
+    }
+    pub fn resize(&mut self,shape:(usize,usize)){
+        self.data=self.reshape(shape).data;
+    }
+    pub fn diagonal(&self)->Matris<T>{
+        let (mut i,mut j)=(0 as usize,self.shape.1);
+        let mut v=Vec::<Vec<T>>::new();
+        for _ in 0..j{
+            v.push(vec![self.data[i][j].clone()]);
+        }
+        let length=v.len();
+        Matris { data: v, shape: (length,1 ) }
+    }
+    pub fn dot(&self,x:Matris<T>)->Matris<T>{
+        if self.shape.0!=x.shape.1 || self.shape.1!=x.shape.0{
+            panic!("shapes {:?} and {:?} not aligned: {} (dim 1) != {} (dim 0)"
+            ,self.shape,x.shape,self.shape.0,x.shape.1)
+        }
+        todo!("todo multiplications");
+        x
     }
 }
